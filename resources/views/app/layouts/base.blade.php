c3f571d0bd75d579fd760e968bbdb43784eeb558
xugf <345632369@qq.com>
2017-09-15 17:45:03 +0800

    no message

--- resources/views/app/layouts/base.blade.php
+++ resources/views/app/layouts/base.blade.php
@@ -77,8 +77,6 @@
             <div class="footer-content">
                 <div class="footer-border">
                     <p><a href="http://www.miitbeian.gov.cn/" target="_blank" rel="nofollow">{{ bloginfo('icp') }}</a></p>
-                    <p>本站点采用<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh" rel="nofollow">知识共享 署名-非商业性使用-相同方式共享 4.0 国际 许可协议</a></p>
-                    <p>Coder by <a href="{{ url('/') }}">@Mr柿子</a>，您可以在 GitHub <i class="fa fa-github"></i> 找到<a href="https://github.com/Cong5/myPersimmon">本站源码</a> - CopyRight &copy; 2017</p>
                     @yield('footer')
                 </div>
             </div>
